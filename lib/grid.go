1bcc715ce35b7f473ac2be47a0459b010025727f
tpacent <76945699+tpacent@users.noreply.github.com>
2024-01-03 22:32:23 +0300

    implement generic grid struct

---
+++ lib/grid.go
@@ -0,0 +1,91 @@
+package lib
+
+import "fmt"
+
+func NewGrid[T any](dimensionSizes ...int) *Grid[T] {
+	var zero T
+	var size = Mul(dimensionSizes...)
+
+	return &Grid[T]{
+		data: make([]T, size),
+		dims: dimensionSizes,
+		zero: zero,
+	}
+}
+
+// Grid is a generic container for n-dimensional grids
+type Grid[T any] struct {
+	data []T
+	dims []int
+	zero T // cached helper value to return in case of error
+}
+
+// Get returns a value at specified coordinates
+func (grid *Grid[T]) Get(coords ...int) (T, error) {
+	index, err := grid.Index(coords...)
+	if err != nil {
+		return grid.zero, err
+	}
+	return grid.data[index], nil
+}
+
+// Set updates a value at specified coordinates
+func (grid *Grid[T]) Set(value T, coords ...int) error {
+	index, err := grid.Index(coords...)
+	if err != nil {
+		return err
+	}
+	grid.data[index] = value
+	return nil
+}
+
+// Index converts coordinates x, y, z, ... into a number
+func (grid *Grid[T]) Index(coords ...int) (index int, err error) {
+	if err = grid.validateCoords(coords...); err != nil {
+		return
+	}
+	for dIndex, dValue := range coords {
+		index += dValue * Mul(grid.dims[:dIndex]...)
+	}
+	return
+}
+
+// Coords converts index to a corresponding set of coords
+func (grid *Grid[T]) Coords(index int) ([]int, error) {
+	if err := grid.validateIndex(index); err != nil {
+		return nil, err
+	}
+	coords := make([]int, len(grid.dims))
+	// most significant coordinate last,
+	// as in: x, y, z.
+	for k := len(grid.dims) - 1; k > -1; k-- {
+		planeSize := Mul(grid.dims[:k]...)
+		coords[k] = index / planeSize
+		index -= coords[k] * planeSize
+	}
+	return coords, nil
+}
+
+// validateIndex index validation routines
+func (grid *Grid[T]) validateIndex(index int) error {
+	if index < 0 {
+		return fmt.Errorf("negative index not allowed")
+	}
+	if index < len(grid.data) {
+		return nil
+	}
+	return fmt.Errorf("index too large")
+}
+
+// validateCoords coordinate set validation routines
+func (grid *Grid[T]) validateCoords(coords ...int) error {
+	if len(coords) != len(grid.dims) {
+		return fmt.Errorf("unexpected coords len")
+	}
+	for index, c := range coords {
+		if c < 0 || c >= grid.dims[index] {
+			return fmt.Errorf("coord %d out of range", index)
+		}
+	}
+	return nil
+}
---
+++ lib/grid_test.go
@@ -0,0 +1,35 @@
+package lib_test
+
+import (
+	"aoc2023/lib"
+	"slices"
+	"testing"
+)
+
+func TestGrid(t *testing.T) {
+	grid := lib.NewGrid[int]([]int{3, 3, 3}...)
+
+	{
+		actual, err := grid.Index(2, 1, 2)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if actual != 23 {
+			t.Error("unexpected value", 23)
+		}
+	}
+
+	{
+		actual, err := grid.Coords(23)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !slices.Equal(actual, []int{2, 1, 2}) {
+			t.Error("unexpected value")
+		}
+	}
+}
